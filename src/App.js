63bf8457f33ef870f8b876f644d68562c0a6b1cd
Ryan McGuire <ryan.mcguire.va@gmail.com>
2022-11-26 18:14:39 -0500

    React Logic Setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import logo from './logo.svg';
+import Heading from './Heading'
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
+
+  const [advice, setAdvice] = useState('')
+  const [chosenAdvice, setChosenAdvice] = useState(1)
+  // const [name, setName] = useState('Generate Name')
+
+  const getAdviceNum = () => {
+    setChosenAdvice(Math.floor((Math.random() * 200) + 1))
+  }
+
+  useEffect(() => {
+    const randomNum = Math.floor((Math.random() * 200) + 1)
+    console.log(`Connected number is ${randomNum}`)
+    axios.get(`https://api.adviceslip.com/advice/${randomNum}`)
+        .then(res => {
+          setAdvice(res.data.slip.advice)
+        })
+  }, [])
+
+  const handleClick = (e) => {
+    getAdviceNum()
+    console.log(`Number updated to ${chosenAdvice}`)
+    axios.get(`https://api.adviceslip.com/advice/${chosenAdvice}`)
+        .then(res => {
+          setAdvice(res.data.slip.advice)
+        })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Heading msg={advice}/> 
+        <button onClick={handleClick}>Gimme Another</button>
       </header>
     </div>
   );
---
+++ src/Heading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Heading = (props) => {
+
+    return(
+        <h3>{props.msg}</h3>
+    )
+}
+
+export default Heading
